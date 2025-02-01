90a5f4c999d83485f58b3e0e69fbb149aa107a81
mstosio <your-GitHub-private-email@users.noreply.github.com>
2025-02-01 15:01:46 +0100

    add prettier

--- src/Ship.js
+++ src/Ship.js
@@ -1,32 +1,35 @@
-import  ShipImagePng  from './assets/ship.png'
-import {ctx} from "./main.js";
+import ShipImagePng from "./assets/ship.png";
+import { ctx } from "./main.js";
 
 export class Ship {
-    constructor({
-                    width, height, positionX, positionY
-                }) {
-        this.width = width;
-        this.height = height
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.image = new Image()
+  constructor({ width, height, positionX, positionY }) {
+    this.width = width;
+    this.height = height;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.image = new Image();
 
-        this.addImageUrl(ShipImagePng)
-        this.drawImage(this.positionX, this.positionY)
+    this.addImageUrl(ShipImagePng);
+    this.drawImage(this.positionX, this.positionY);
+  }
+
+  addImageUrl(imageUrl) {
+    if (this.image) {
+      this.image.src = imageUrl;
     }
+  }
 
-    addImageUrl(imageUrl){
-        if(this.image){
-            this.image.src = imageUrl
-        }
+  drawImage(positionX, positionY) {
+    if (this.image) {
+      this.image.onload = () => {
+        ctx.drawImage(
+          this.image,
+          positionX,
+          positionY,
+          this.width,
+          this.height,
+        );
+      };
     }
-
-    drawImage(positionX, positionY){
-        if(this.image) {
-            this.image.onload = () => {
-                ctx.drawImage(this.image, positionX, positionY, this.width, this.height)
-            }
-        }
-    }
-
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
-import {Ship} from "./Ship.js";
-import {SHIP_HEIGHT, SHIP_WIDTH} from "./gameConfig.js";
+import { Ship } from "./Ship.js";
+import { SHIP_HEIGHT, SHIP_WIDTH } from "./gameConfig.js";
 
-export const canvas = document.getElementById("chicken-invaders-canvas")
-export const ctx = canvas.getContext('2d')
+export const canvas = document.getElementById("chicken-invaders-canvas");
+export const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const ship = new Ship({
-    width: SHIP_WIDTH,
-    height: SHIP_HEIGHT,
-    positionX: canvas.width / 2,
-    positionY: canvas.height - 75
-})
-
-
-
-
-
+// const ship = new Ship({
+//     width: SHIP_WIDTH,
+//     height: SHIP_HEIGHT,
+//     positionX: canvas.width / 2,
+//     positionY: canvas.height - 75
+// })
